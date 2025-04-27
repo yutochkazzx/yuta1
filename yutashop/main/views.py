0ef6a6e9d33621eed1ebf949467e8c6043ec1158
yuta <iiitoobee@gmail.com>
2025-04-27 13:13:44 +0300

    new models/new view interface

--- yutashop/main/views.py
+++ yutashop/main/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.views.generic import ListView,DetailView
+from .models import Category, ClothingItems, Size
+from django.db.models import Q
 
-# Create your views here.
+class CatalogView:
+    model = ClothingItems
+    template_name = 'main/product/list.html'
+    context_object_name = 'clothing_items'
+
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category_slugs = self.request.GET.getlist('category')
+        size_names = self.request.GET.getlist('size')
+        min_price = self.request.GET.get('min_price')
+        max_price = self.request.GET.get('max_price')    
+
+
+        if category_slugs:
+            queryset = queryset.filter(category__slugs__in = category_slugs)  
+
+        if size_names:
+            queryset = queryset.filter(
+            Q(size__name__in = size_names ) & Q(sizes__clothingitemsize__avaliable = True)
+            )
+
+        if min_price:
+            queryset = queryset.filter(price__gte=min_price)
+        if max_price:
+            queryset = queryset.filter(price__lte=max_price)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['sizes'] = Size.objects.all()
+        context['selected_categories'] = self.request.GET.getlist('category')
+        context['selected_sizes'] = self.request.GET.getlist('size')
+        context['min_price'] =  self.request.GET.get('min_price', '')
+        context['max_price'] = self.request.GET.get('min_price', '')
+
+        return context
+    
+class ClothingItemDetailView(DetailView):
+    model = ClothingItems
+    template_name = 'main/product/detail.html'
+    context_object_name = 'clothing_item'
+    slug_field = 'slug'
+    slug_url_kwarg = 'slug'
+
+
+
+        
